ece3f087cf65f01f6c26c4fb6941e8ecc55e08fc
joseavc <joseavillalobos1997@gmail.com>
2022-07-13 00:39:16 -0500

    Breaking Bad app finished

---
+++ src/components/ui/Search.js
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+
+function Search({ getQuery }) {
+  const [text, setText] = useState('');
+
+  const onChange = (q) => {
+    setText(q);
+    getQuery(q);
+  };
+  return (
+    <section className="search">
+      <form action="">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search characters"
+          value={text}
+          onChange={(e) => onChange(e.target.value)}
+          autoFocus
+        />
+      </form>
+    </section>
+  );
+}
+
+export default Search;
